5a3bd5aaf5b04b1bcf9bb924bea64a81174a59e6
waraich1 <waraich@mcmaster.ca>
2021-01-20 17:32:02 -0500

    Optimal Algo

--- code.py
+++ code.py
@@ -1,13 +1,14 @@
 def are_valid_groups(a, b):
-    for i in a:
-        indicate = 0
-        for j in b:
-            if i in j:
-                indicate = 1
-                break
-        if indicate == 0:
+    hashTable = set()
+    for i in b:
+        for j in i:
+            hashTable.add(j)
+    for k in a:
+        if k not in hashTable:
             return False
+    return True	
+  
 
-    return True
 
-
+#print(are_valid_groups(["1", "2"], [["1", "3"], ["2", "4"]]))
+#print(are_valid_groups(["1", "2"], [["5", "3"], ["2", "4"]]))
